2999732c69f4d58704e47935252d80669a7d6739
Syed Saleem Raza Shah <syedsaleemrazashah69@gmail.com>
2024-06-26 13:09:35 +0500

    error

---
+++ app.js
@@ -0,0 +1,48 @@
+const columns = document.querySelectorAll(".column");
+
+const createTicket = (value) => {
+  //
+  const ticket = document.createElement("p");
+  const elementText = document.createTextNode(value);
+
+  ticket.setAttribute("draggable", "true");
+  ticket.appendChild(elementText);
+
+  return ticket;
+};
+
+let savedTasks = JSON.parse(localStorage.getItem("savedTasks")); // fetching savedTasks obj and converting
+
+if (!savedTasks) {
+  savedTasks = {};
+}
+
+const addTask = (event) => {
+  event.preventDefault();
+
+  const currentForm = event.target; // current form element
+  const value = currentForm.elements[0].value; // value written in form's input
+  const parent = currentForm.parentElement; // parent of form i.e div.column
+  const ticket = createTicket(value); // div to be added
+
+  parent.insertBefore(ticket, currentForm); // adding new task before the form
+
+  const h3Value = parent.children[0].innerText;
+
+  if (!Array.isArray(savedTasks[h3Value])) {
+    // agar array nhi hy tw khali array set karwa do kyu ky undefined ma .push() nhi ho sagta
+    savedTasks[h3Value] = [];
+  }
+
+  savedTasks[h3Value].push(value);
+
+  localStorage.setItem("savedTasks", JSON.stringify(savedTasks)); // saving data after adding each task
+
+  currentForm.reset(); // clearing form
+};
+
+for (let i = 0; i < columns.length; i++) {
+  const form = columns[i].lastElementChild; // selecting every column's form because form is last element
+
+  form.addEventListener("submit", addTask);
+}
